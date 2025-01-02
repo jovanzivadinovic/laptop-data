60657a745ae6a5ba970852adcba4121f7a513674
jovanzivadinovic <jovanzivadinovic2000@outlook.com>
2025-01-02 12:15:00 +0100

    update

--- kmeans_cluster.py
+++ kmeans_cluster.py
@@ -6,11 +6,13 @@ from sklearn.preprocessing import StandardScaler
 
 
 class KMeansCluster:
-    def find_optimal_k(self, df):
+    def find_optimal_k(self, X):
+        scaler = StandardScaler()
+        X_scaled = scaler.fit_transform(X)
         wcss = []
         for i in range(1, 11):
             kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
-            kmeans.fit(df)
+            kmeans.fit(X_scaled)
             wcss.append(kmeans.inertia_)
         plt.plot(range(1, 11), wcss)
         plt.title('Elbow Method')
@@ -18,4 +20,7 @@ class KMeansCluster:
         plt.ylabel('WCSS')
         plt.show()
     
+    def kmeans(self, X, n_clusters):
+        plt.scatter(X[:, 0], X[:, 1], c=n_clusters)
+        plt.show()
     
