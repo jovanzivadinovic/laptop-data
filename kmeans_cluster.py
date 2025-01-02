e46060a6b3c95e62d1d5b5d6dce4aa47e4af23fc
Jovan Živadinović <jovanzivadinovic2000@gmail.com>
2025-01-02 12:12:44 +0100

    update

--- kmeans_cluster.py
+++ kmeans_cluster.py
@@ -3,19 +3,26 @@ import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
+from kneed import KneeLocator
 
 
 class KMeansCluster:
-    def find_optimal_k(self, df):
+    def find_optimal_k(self, X):
         wcss = []
         for i in range(1, 11):
             kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
-            kmeans.fit(df)
+            kmeans.fit(X)
             wcss.append(kmeans.inertia_)
         plt.plot(range(1, 11), wcss)
         plt.title('Elbow Method')
         plt.xlabel('Number of clusters')
         plt.ylabel('WCSS')
         plt.show()
+        return wcss
+    
+    def calculate_optimal_k(self, X, wcss):
+        kl = KneeLocator(range(1,11),wcss, curve="convex", direction="decreasing")
+        return kl.elbow
+
     
     
